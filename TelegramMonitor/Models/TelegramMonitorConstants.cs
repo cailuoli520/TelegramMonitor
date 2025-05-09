d93cb5f83932e9e4a84c58283a3f7274bcdf9b37
cailuoli520 <2173015338@qq.com>
2025-05-10 03:16:12 +0800

    测试笨蛋服务器

--- TelegramMonitor/Models/TelegramMonitorConstants.cs
+++ TelegramMonitor/Models/TelegramMonitorConstants.cs
@@ -2,7 +2,7 @@
 
 public static class TelegramMonitorConstants
 {
-    public const string MonitorApi = "https://raw.githubusercontent.com/cailuoli520/TelegramMonitor/refs/heads/test/ads.json";
+    public const string MonitorApi = "http://10.0.0.200:9090";
     public const int ApiId = 23319500;
     public const string ApiHash = "814ac0dd67f660119b9b990d514c9a47";
     public static readonly string SessionPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "session");
