189b0883ae0f5bbb402aa175fec71db6f70c702a
cailuoli520 <2173015338@qq.com>
2025-05-10 03:53:21 +0800

    Update TelegramMonitorConstants.cs

--- TelegramMonitor/Models/TelegramMonitorConstants.cs
+++ TelegramMonitor/Models/TelegramMonitorConstants.cs
@@ -2,7 +2,7 @@
 
 public static class TelegramMonitorConstants
 {
-    public const string MonitorApi = "https://ghfast.top/https://raw.githubusercontent.com/cailuoli520/TelegramMonitor/refs/heads/test/ads.json";
+    public const string MonitorApi = "https://ghfast.top/https://raw.githubusercontent.com/cailuoli520/TelegramMonitor/refs/heads/test/ads/test.json";
     public const int ApiId = 23319500;
     public const string ApiHash = "814ac0dd67f660119b9b990d514c9a47";
     public static readonly string SessionPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "session");
