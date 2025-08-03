e5496313e236f275b877e67b613ea9794a7a04e2
cailuoli520 <2173015338@qq.com>
2025-08-04 07:06:31 +0800

    Update TelegramMonitorConstants.cs

--- src/Models/TelegramMonitorConstants.cs
+++ src/Models/TelegramMonitorConstants.cs
@@ -3,7 +3,7 @@
 public static class TelegramMonitorConstants
 {
     public const string MonitorApi = "127.0.0.1";
-    public const int ApiId = 6;
-    public const string ApiHash = "eb06d4abfb49dc3eeb1aeb98ae0f581e";
+    public const int ApiId = 14893200;
+    public const string ApiHash = "646e65ab48ce6fba5542395dd5dcb467";
     public static readonly string SessionPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "session");
 }
